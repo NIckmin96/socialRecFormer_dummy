b0014b90e70daab5bab747f49c2cfd27f3f9100a
sdsf1225 <sdsf1225@gmail.com>
2025-01-16 16:34:47 +0900

    fix: lr-eps

--- main.py
+++ main.py
@@ -612,7 +612,7 @@ def main():
         mode = 'min',
         factor = 0.5,
         patience = 2,
-        eps = 1e-3,
+        eps = 1e-2,
         # min_lr = 5e-7,
         verbose = True
     )
