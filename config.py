0b5edaaf1b4fecc6fc3422239bd598a112d673b4
BK <sdsf1225@gmail.com>
2025-10-04 10:54:24 +0900

    tmp

--- config.py
+++ config.py
@@ -70,7 +70,7 @@ yelp={
             "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim num_workers
             "d_ffn": 256,            # FFN dim
             "num_heads": 2,
-            "dropout": 0.3,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
+            "dropout": 0.1,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
             "n_experts": 8,
             "topk": 2
          },
@@ -82,73 +82,35 @@ yelp={
 
 douban={
          "model":{
-            "num_user": 75710,
-            "num_item": 114854,
-            "max_user_degree": 4118,
-            "max_item_degree": 717,
-            "max_spd_value": 15,
             "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim num_workers
             "d_ffn": 256,            # FFN dim
             "num_heads": 4,
             "dropout": 0.1,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
-            "num_layers_enc": 3,
-            "num_layers_dec": 5,
             "n_experts": 8,
             "topk": 1
          },
          "training":{
-            "batch_size":128,        # total_train_step: 835 (1 epoch 당 `len(train_dataset) / batch_size`)
-            "optimizer":"adamw",
-            "learning_rate":0.01,
-            "warmup":80, 
-            "lr_decay":"cos",
             "weight_decay":1e-1,
             "eval_frequency":400, 
-            "num_epochs":100,
-            "num_eval_steps":849,   # total_valid_sample / total_epoch
-            "patience":10, 
-            "alpha":1,
-            "beta":3,
-            "gamma":3,
-            "baseline_mae":0.8383,
-            "baseline_rmse":1.0972
+            "num_epochs":300
          },
      },
 
 Douban={
          "model":{
-            "num_user": 72000,
-            "num_item": 80269,
-            "max_user_degree": 2982,
-            "max_item_degree": 21316,
-            "max_spd_value": 15,
             "d_model": 64,          # MHA dim (Linear modules in Attention Network) & Embedding dim num_workers
             "d_ffn": 256,            # FFN dim
             "num_heads": 4,
-            "dropout": 0.3,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
-            "num_layers_enc": 3,
-            "num_layers_dec": 5,
+            "dropout": 0.1,         # Inside FFN, decoder_layer & encoder_layer (applied after linear & attention)
             "n_experts": 8,
-            "topk": 2
+            "topk": 1
          },
          "training":{
-            "batch_size":128,        # total_train_step: 835 (1 epoch 당 `len(train_dataset) / batch_size`)
-            "optimizer":"adamw",
-            "learning_rate":0.01,
-            "warmup":80, 
-            "lr_decay":"cos",
-            "weight_decay":1e-2,
+            "weight_decay":1e-1,
             "eval_frequency":400, 
-            "num_epochs":300,
-            "num_eval_steps":849,   # total_valid_sample / total_epoch
-            "patience":10, 
-            "alpha":1,
-            "beta":3,
-            "gamma":3,
-            "baseline_mae":0.8383,
-            "baseline_rmse":1.0972
+            "num_epochs":300
          },
-     }
+     },
 
 
 
@@ -157,6 +119,5 @@ Config = {
     "ciao_timestamp":ciao_timestamp,
     "epinions":epinions,
     "yelp":yelp,
-    "douban":douban,
     "Douban":Douban
 }
